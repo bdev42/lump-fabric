4c87efffddf69e36656a5dc1fe95b145dc7d94ce
bdev42 <140114350+bdev42@users.noreply.github.com>
2025-02-26 00:14:12 +0100

    Add helpful tooltip to AmethystGoggles

--- src/main/java/com/github/bdev42/lump/item/AmethystGoggles.java
+++ src/main/java/com/github/bdev42/lump/item/AmethystGoggles.java
@@ -1,8 +1,13 @@
 package com.github.bdev42.lump.item;
 
+import net.minecraft.client.item.TooltipType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Equipment;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+
+import java.util.List;
 
 public class AmethystGoggles extends Item implements Equipment {
 
@@ -17,5 +22,18 @@ public class AmethystGoggles extends Item implements Equipment {
     @Override
     public EquipmentSlot getSlotType() {
         return EquipmentSlot.HEAD;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.translatable("item.lump.amethyst_goggles.tooltip"));
+        if (options.isAdvanced()) {
+            tooltip.add(Text.empty());
+            tooltip.add(Text.translatable("item.lump.amethyst_goggles.tooltip2.advanced"));
+            tooltip.add(Text.translatable("item.lump.amethyst_goggles.tooltip3.advanced"));
+            tooltip.add(Text.translatable("item.lump.amethyst_goggles.tooltip4.advanced"));
+            tooltip.add(Text.translatable("item.lump.amethyst_goggles.tooltip5.advanced"));
+            tooltip.add(Text.translatable("item.lump.amethyst_goggles.tooltip6.advanced"));
+        }
     }
 }
